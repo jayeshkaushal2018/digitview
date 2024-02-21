b64823843decbf5264d7160516ceac9ef6597f1a
Madhukar <singhmadhukar11@gmail.com>
2024-02-21 22:02:40 +0530

    fix: added footer icons

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -8,7 +8,7 @@ function Breadcrumb(props) {
     return (<>
      <Container>
         <div style={{ marginTop: "90px"}}>
-            <div className='container my-5'>
+            <div className='container mt-5'>
                 <div className='row'>
                     <div className='d-fluid'>
                         <p style={{ fontWeight: "bold", "color": "#0778e2"}}>{props.value1} <FaAngleRight /> <Link to={props.value4}  style={{ "textDecoration": 'none' ,"color": "#0778e2"}}> {props.value2}</Link> <FaAngleRight />{props.value3} </p>
